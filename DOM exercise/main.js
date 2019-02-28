7d49c0e0d0db11d474de8cc2c8f22f8aa6d551d4
Meegan Schafer <45988365+schafermeegan@users.noreply.github.com>
2019-02-27 19:14:38 -0500

    Add files via upload

--- DOM exercise/main.js
+++ DOM exercise/main.js
@@ -1,7 +1,13 @@
 function makeRecipe() {
-    var strText = document.getElementById("exampleFormControlTextarea1").nodeValue;
-
-    var result = strText;
+    let strText = document.getElementById("exampleFormControlTextarea1").value;
+    let strText1 = document.getElementById("exampleFormControlInput1").value;
+    let strText2 = document.getElementById("exampleFormControlTextarea2").value;
+    
+    let result = [strText, strText1, strText2]
     document.getElementById(`spanResult`).textContent = result;
+
+    
+
+   
 
 }
